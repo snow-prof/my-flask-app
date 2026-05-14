2577f0e3c30ba93b25f4c2b67c0255111f48b7f3
JSY <snowflower@knu.ac.kr>
2026-05-15 07:54:08 +0900

    fix: error E302

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.utils import is_positive
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.get('/')
 def hello_world():
     # 시연 중 텍스트를 수정하여 캐싱 및 업데이트 시연용으로 사용
     result = is_positive(10)
