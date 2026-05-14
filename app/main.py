04400035c57f589d4ea40325e05bd84d534d8a39
JSY <snowflower@knu.ac.kr>
2026-05-15 07:50:29 +0900

    fix: use func

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # 시연 중 텍스트를 수정하여 캐싱 및 업데이트 시연용으로 사용
-    return 'welcome 2!'
+    result = is_positive(10)
+    return f'welcome {result}!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
